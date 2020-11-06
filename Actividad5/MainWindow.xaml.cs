baf3efc692a267c5e2a51bdf8693aa1bd6ba5e7c
alumnot1 <alumnot1@ORD12>
2020-11-06 11:45:49 +0100

    Añado los botones en el code behind

--- Actividad5/MainWindow.xaml.cs
+++ Actividad5/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Actividad5
 {
@@ -7,6 +8,30 @@ namespace Actividad5
         public MainWindow()
         {
             InitializeComponent();
+            const int FILAS = 3, COLUMNAS = 3;
+            int numero = 1;
+            for (int i = 1; i <= FILAS; i++)
+            {
+                for (int j = 0; j < COLUMNAS; j++)
+                {
+                    Button boton = new Button
+                    {
+                        Tag = numero,
+                        Content = new Viewbox
+                        {
+                            Child = new TextBlock
+                            {
+                                Text = numero.ToString()
+                            }
+                        }
+                    };
+                    Grid.SetRow(boton, i);
+                    Grid.SetColumn(boton, j);
+
+                    numeros.Children.Add(boton);
+                    numero++;
+                }
+            }
         }
     }
 }
